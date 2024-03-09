7e871c3c0d565f2c35d9c8274ea4d8dd17833953
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2024-03-09 22:07:15 +0100

    Atomes doxygen website build: sam. 09 mars 2024 22:07:15 CET

---
+++ search/functions_18.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['zoom_8701',['zoom',['../dc/d21/glview_8c.html#a7e0dc62ec09bf3ea0bea31b2e722495d',1,'zoom(glwin *view, int delta):&#160;glview.c'],['../dc/de3/glview_8h.html#a7e0dc62ec09bf3ea0bea31b2e722495d',1,'zoom(glwin *view, int delta):&#160;glview.c']]]
+];
